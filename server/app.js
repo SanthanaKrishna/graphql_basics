334b8845c965b6ddd38e4f776473218ada968565
Santhana Krishna <36031304+SanthanaKrishna@users.noreply.github.com>
2019-06-24 23:29:44 +0530

    started learn

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = required('express');
+const graphqlHTTP = require('express-graphql');
+const schema= require('../schema/schema');
+const app = express();
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+
+app.listen('4000', () => {
+    console.log('Server Started')
+})
