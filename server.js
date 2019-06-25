670ad5ccd0715649b28cc5676fe5fbc2904f1232
Santhana Krishna <SanthanaKrishna96@users.noreply.github.com>
2019-06-25 14:40:33 +0530

    imp: RootQuery and mutation,
    imp: RootQuery and mutation in graphlql and connect mongodb to save the data through mutation

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema= require('./schema/schema');
+const mongoose = require('mongoose');
+const schema = require('./server/schema/schema');
+
+mongoose.connect('mongodb+srv://graphql_Book:wrKHnQ1p4jSd8m8T@cluster0-h5kgo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .then(() => {
+        console.log('try mongodb connection');
+    }).catch(err => {
+        console.log(err);
+    })
+
+mongoose.connection.once('open', () => {
+    console.log('database is coonected with mongodb')
+})
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql:true
+    graphiql: true
 }))
 
 app.listen('4000', () => {
